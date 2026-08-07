abddda072f9992119d55aae90a5151cd08c971e6
leozin94 <leonardo.monteiro.almeida@escola.pr.gov.br>
2026-08-07 09:07:17 -0300

    ok

---
+++ script.js
@@ -0,0 +1,26 @@
+//Botões de acessibilidade
+document.addEventListener("DOMContentLoaded, () =>{
+
+const btnContraste = document.getElementById("btn-contraste");
+const btnAumentar = document.getElementById("btn-aumentar-texto");
+const btnDiminuir = document.getElementById("btn-diminuir-texto");
+
+//aumentar a fonte
+
+let tamanhoAtualfonte = 100;
+
+//função alto contraste
+
+btnContraste.addEventListener("click",() =>{
+
+document.body.classList.toogle("alto-contraste");
+
+//acessibilidade para leitores de tela
+
+const ativo = document.body.classList.constains("alto-contraste");
+btnContraste.setAttribute("aria-pressed", ativo);
+
+)
+
+
+}"}
